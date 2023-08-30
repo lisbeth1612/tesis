bc74a6db9826ea9619405cd7286bb337c82c2f06
Rubenamesaf <rubenamesaf@gmail.com>
2023-08-30 17:30:43 -0400

    Primera version del proyecto

---
+++ lib/utils/global.colors.dart
@@ -0,0 +1,6 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  static HexColor mainColor = HexColor('#1E19D0');
+  static HexColor textColor = HexColor('#4F4F4F');
+}
---
+++ lib/view/widgets/social.login.dart
@@ -0,0 +1,53 @@
+ import 'package:flutter/material.dart';
+import 'package:login_v1/utils/global.colors.dart';
+//import 'package:login_v1/utils/global.colors.dart';
+//import 'dart:async';
+//import 'package:get/get.dart';
+//import 'package:login_v1/view/login.view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+ class SocialLogin extends StatelessWidget {
+  const SocialLogin({Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: Text(
+          '-Or sing in with-',
+          style: TextStyle(
+            color: GlobalColors.textColor,
+            fontWeight: FontWeight.w600,
+          ),
+              ),
+        ),
+         const SizedBox(height: 15),
+         Row(children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(6),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(.1),
+                    blurRadius: 10
+                  )
+                ],
+              ),
+              child: SvgPicture.asset(
+                'assets/images/bxs-home.svg',
+                 height: 30,
+                 ),
+            ),
+          )
+         ],
+         )
+      ],
+    );
+  }
+ }
