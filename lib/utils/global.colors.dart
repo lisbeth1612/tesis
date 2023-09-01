5a5c9b6cdbb465554cc2a14ec22028c4bd67e49d
lisbeth1612 <81590513+lisbeth1612@users.noreply.github.com>
2023-09-01 10:48:30 -0400

    2da version final

--- lib/utils/global.colors.dart
+++ lib/utils/global.colors.dart
@@ -1,6 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class GlobalColors {
-  static HexColor mainColor = HexColor('#1E19D0');
-  static HexColor textColor = HexColor('#4F4F4F');
+  //este es para pantalla 1 el main
+  static HexColor mainColor = HexColor('#FFC59B');
+  static HexColor textColor = HexColor('#101470');
+  static HexColor blancoColor = HexColor('#FFFFFF');
+  static HexColor negroColor = HexColor('#000000');
+  static HexColor grayColor = HexColor('#ADBACEC7');
+  static HexColor naranjaColor = HexColor('#ED9A5E');
+  static HexColor narajanOscuroColor = HexColor('#F29757DB');
+  static const amarilloColor = Color(0xD1FBE288);
+  static const naranjaClaritoColor = Color(0xF2FEC49A);
 }
